477be71002b9f39362a76435906a3c23af199fff
HuajiSoup <HUAJIsoup233@163.com>
2025-10-02 17:49:26 +0800

    Resizer completed

--- assets/comp/resizer/resizer.js
+++ assets/comp/resizer/resizer.js
@@ -1,27 +1,38 @@
+window.pressing = false;
+window.pressingFunction = null;
+
+document.addEventListener("mouseup", () => {
+    if (window.pressing) {
+        window.pressing = false;
+        document.removeEventListener("mousemove", window.pressingFunction);
+        window.pressingFunction = null;
+    }
+})
+
 function addResizer(elem, horizonal = true, minkeep = 0) {
     let resizer = document.createElement("div");
     resizer.classList.add("resizer");
     resizer.setAttribute("data-dragging", "0");
 
-    let _moveHandler;
+    let moveHandler;
     if (horizonal) {
         resizer.classList.add("hrz");
-        _moveHandler = (e) => {
+        moveHandler = (e) => {
             elem.style.width = e.clientX - elem.offsetLeft + "px";
         };
     } else {
         resizer.classList.add("vtc");
-        _moveHandler = (e) => {
+        moveHandler = (e) => {
             elem.style.height = e.clientY - elem.offsetTop + "px";
         };
     }
-    let moveHandler = (e) => { _moveHandler(e); }
 
     resizer.addEventListener("mousedown", () => {
-        document.addEventListener("mousemove", moveHandler);
-    });
-    resizer.addEventListener("mouseup", () => {
-        document.removeEventListener("mousemove", moveHandler);
+        if (!window.pressing) {
+            window.pressing = true;
+            window.pressingFunction = moveHandler;
+            document.addEventListener("mousemove", moveHandler);
+        }
     });
 
     elem.after(resizer);
